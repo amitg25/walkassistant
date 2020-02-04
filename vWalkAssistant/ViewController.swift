29a4a95110a00c601b8d4c04f511d7269912db5e
amgupta <amgupta@vmware.com>
2020-02-03 20:48:29 -0800

    Initial Commit

---
+++ vWalkAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vWalkAssistant
+//
+//  Created by Amit Gupta on 2/3/20.
+//  Copyright © 2020 Amit Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
